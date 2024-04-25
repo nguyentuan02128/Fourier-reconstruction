fc1f3dd93aff1565992a21d8247bfe7391fd6357
nguyentuan02128 <168062947+nguyentuan02128@users.noreply.github.com>
2024-04-25 21:18:15 +0700

    Add files via upload

--- backend.py
+++ backend.py
@@ -103,23 +103,21 @@ def get_latex(filename):
             if segment.is_corner:
                 x1, y1 = segment.c
                 x2, y2 = segment.end_point
-                coordinates.append('((1-t)%f+t%f,(1-t)%f+t%f)' % (x0, x1, y0, y1))
+                coordinates.append(f'({x0}, {x1}, {y0}, {y1})')
                 latex.append('((1-t)%f+t%f,(1-t)%f+t%f)' % (x0, x1, y0, y1))
-                coordinates.append('((1-t)%f+t%f,(1-t)%f+t%f)' % (x1, x2, y1, y2))
+                coordinates.append(f'({x1}, {x2}, {y1}, {y2})')
                 latex.append('((1-t)%f+t%f,(1-t)%f+t%f)' % (x1, x2, y1, y2))
             else:
                 x1, y1 = segment.c1
                 x2, y2 = segment.c2
                 x3, y3 = segment.end_point
-                coordinates.append('((1-t)((1-t)((1-t)%f+t%f)+t((1-t)%f+t%f))+t((1-t)((1-t)%f+t%f)+t((1-t)%f+t%f)),\
-                (1-t)((1-t)((1-t)%f+t%f)+t((1-t)%f+t%f))+t((1-t)((1-t)%f+t%f)+t((1-t)%f+t%f)))' % \
-                (x0, x1, x1, x2, x1, x2, x2, x3, y0, y1, y1, y2, y1, y2, y2, y3))
+                coordinates.append(f'({x0}, {x1}, {x1}, {x2}, {x1}, {x2}, {x2}, {x3}, {y0}, {y1}, {y1}, {y2}, {y1}, {y2}, {y2}, {y3})')
                 latex.append('((1-t)((1-t)((1-t)%f+t%f)+t((1-t)%f+t%f))+t((1-t)((1-t)%f+t%f)+t((1-t)%f+t%f)),\
                 (1-t)((1-t)((1-t)%f+t%f)+t((1-t)%f+t%f))+t((1-t)((1-t)%f+t%f)+t((1-t)%f+t%f)))' % \
                 (x0, x1, x1, x2, x1, x2, x2, x3, y0, y1, y1, y2, y1, y2, y2, y3))
             start = segment.end_point
     print('-----------------------------')
-    # print(coordinates)
+    print(coordinates)
     with open('coordinates.txt', 'w') as file:
         for coord in coordinates:
             file.writelines(coord + '\n')
