c8be07f5c0229a4f80b4c644ac42e72c06286448
ThuyBachDuong <trandac1415@gmail.com>
2024-05-01 07:33:18 +0700

    Get curve split points.

--- conjoin.py
+++ conjoin.py
@@ -1,31 +1,5 @@
-# with open('coordinates.txt', 'r') as file:
-#     lines = file.readlines()
-
-# def getCoordinatesList(line: str) -> list[float]:
-#     coordinatesList = []
-#     for number in line:
-#         coordinatesList.append(float(number[:-1]))
-#     return coordinatesList
-
-# def getCoordFromList(coordinatesList: list[float]) -> list:
-#     if len(coordinatesList) == 4:
-#         return [(coordinatesList[0], coordinatesList[2]), (coordinatesList[1], coordinatesList[3])]
-#     elif len(coordinatesList) == 16:
-#         return [(coordinatesList[0], coordinatesList[8]), (coordinatesList[7], coordinatesList[15])]
-#     return coordinatesPairs
-        
-# coordinatesPairs = []
-
-# for line in lines:
-#     line = line[1:-3]
-#     line = line.split(' ')
-#     coordinatesList = getCoordinatesList(line)
-#     coordinatesPairs.append(getCoordFromList(coordinatesList))
-
-# def getDistance(pointA: tuple, pointB: tuple) -> float:
-#     return ((pointA[0]-pointB[0])**2 + (pointA[1]-pointB[1])**2)**(0.5)
-
-# print(coordinatesPairs)
+import scipy.integrate as sci
+from scipy.misc import derivative
 
 with open ('curves.txt', 'r') as file:
     lines = file.readlines()
@@ -39,6 +13,7 @@ def getRefinedCurve(curve: str) -> str:
         if curve[pos] == '(':
             if curve[pos-1] == ')' or isDigit(curve[pos-1]) or curve[pos-1] == 't':
                 curve = curve[:pos] + '*' + curve[pos:]
+
     for pos in range(1, len(curve)):
         if curve[pos] == ')':
             if curve[pos+1] == '(' or isDigit(curve[pos+1]) or curve[pos+1] == 't':
@@ -53,48 +28,106 @@ def getRefinedCurve(curve: str) -> str:
     
     return curve
 
-curveCoordinatesPairsList = []
-
-for line in lines:
+def getxCurveAndyCurveFromLine(line):
     line = line[1:-2]
     line = line.replace(' ', '')
     line = line.split(',')
-
-    line_xCurve = getRefinedCurve(line[0])
-    line_yCurve = getRefinedCurve(line[1])
-
-    x_0, y_0 = eval(line_xCurve.replace('t', '0')), eval(line_yCurve.replace('t', '0'))
-    x_1, y_1 = eval(line_xCurve.replace('t', '1')), eval(line_yCurve.replace('t', '1'))
-    
-    curveCoordinatesPairsList.append([(x_0, y_0), (x_1, y_1)])
-
-def getDistance(pointA: tuple, pointB: tuple) -> float:
-    return ((pointA[0]-pointB[0])**2 + (pointA[1]-pointB[1])**2)**(0.5)
-
-arrangedPairsList = [curveCoordinatesPairsList[0]]
-curveCoordinatesPairsList.pop(0)
-
-for _ in range(len(curveCoordinatesPairsList)):
-    d = getDistance(arrangedPairsList[-1][1], curveCoordinatesPairsList[0][0])
-    currentBest = curveCoordinatesPairsList[0]
-
-    for coordinatesPair in curveCoordinatesPairsList:
-        if getDistance(arrangedPairsList[-1][1], coordinatesPair[0]) < d:
-            d = getDistance(arrangedPairsList[-1][1], coordinatesPair[0])
-            currentBest = coordinatesPair
-    arrangedPairsList.append(currentBest)
-    curveCoordinatesPairsList.remove(currentBest)
-
-# print(arrangedPairsList)
+    return line
 
 def getConnectLine(startPoint: tuple, endPoint: tuple) -> str:
     x_0, x_1 = startPoint[0], endPoint[0]
     y_0, y_1 = startPoint[1], endPoint[1]
     return f'((1-t){x_0}+t{x_1}, (1-t){y_0}+t{y_1})'
 
-with open ('newcurves.txt', 'w') as newfile:
-    for n in range(len(lines)):
-        newfile.writelines(lines[n])
-        if n == len(lines) - 1:
-            break
-        newfile.writelines(getConnectLine(arrangedPairsList[n][1], arrangedPairsList[n+1][0]) + '\n')
+def getStartEndPoints():
+    curveCoordinatesPairsList = []
+
+    for line in lines:
+        line_xCurve, line_yCurve = getxCurveAndyCurveFromLine(line)
+
+        x_0, y_0 = eval(line_xCurve.replace('t', '0')), eval(line_yCurve.replace('t', '0'))
+        x_1, y_1 = eval(line_xCurve.replace('t', '1')), eval(line_yCurve.replace('t', '1'))
+        
+        curveCoordinatesPairsList.append([(x_0, y_0), (x_1, y_1)])
+    
+    return curveCoordinatesPairsList
+
+def getDistance(pointA: tuple, pointB: tuple) -> float:
+    return ((pointA[0]-pointB[0])**2 + (pointA[1]-pointB[1])**2)**(0.5)
+
+def connectTheCurves(curveCoordinatesPairsList: list):
+    arrangedPairsList = [curveCoordinatesPairsList[0]]
+    curveCoordinatesPairsList.pop(0)
+
+    for _ in range(len(curveCoordinatesPairsList)):
+        d = getDistance(arrangedPairsList[-1][1], curveCoordinatesPairsList[0][0])
+        currentBest = curveCoordinatesPairsList[0]
+
+        for coordinatesPair in curveCoordinatesPairsList:
+            if getDistance(arrangedPairsList[-1][1], coordinatesPair[0]) < d:
+                d = getDistance(arrangedPairsList[-1][1], coordinatesPair[0])
+                currentBest = coordinatesPair
+        arrangedPairsList.append(currentBest)
+        curveCoordinatesPairsList.remove(currentBest)
+    
+    return arrangedPairsList
+
+def getNewCurvesFile(arrangedPairsList: list) -> None:
+    with open ('newcurves.txt', 'w') as newfile:
+        for n in range(len(lines)):
+            newfile.writelines(lines[n])
+            if n == len(lines) - 1:
+                break
+            newfile.writelines(getConnectLine(arrangedPairsList[n][1], arrangedPairsList[n+1][0]) + '\n')
+
+def splitCurve(xCurve, yCurve, n: int) -> list:
+    splitSteps = []
+    splitPoints = []
+
+    def getDerivative(t: float) -> float:
+        return (xCurve(t)**2 + yCurve(t)**2)**0.5
+
+    I, R = sci.quad(getDerivative, 0, 1)
+    var = 0
+    current = 0
+
+    for k in range(n*1000):
+        I_k, R_k = sci.quad(getDerivative, current, current + 1/(n*1000))
+        var += I_k
+        if abs(I/n - var) < 1 and len(splitSteps) < n-1:
+            splitSteps.append(k/(n*1000))
+            var = 0
+        current += 1/(n*1000)
+    
+    for t in splitSteps:
+        splitPoints.append((xCurve(t), yCurve(t)))
+    return splitPoints
+
+def getSplitPoints():
+    curvesSplitPoints = []
+
+    with open('splitPoints.txt', 'w') as splitPointsFile:
+        for k in range(1732):
+            line = lines[k]
+            # xCurve, yCurve = getxCurveAndyCurveFromLine(line)
+
+            def xCurveFunction(t: float) -> float:
+                xCurve = getxCurveAndyCurveFromLine(line)[0]
+                curve = xCurve
+                curve = getRefinedCurve(str(curve))
+                xCurve = curve.replace('t', f'{t}')
+                return eval(xCurve)
+            
+            def yCurveFunction(t: float) -> float:
+                yCurve = getxCurveAndyCurveFromLine(line)[1]
+                curve = yCurve
+                curve = getRefinedCurve(str(curve))
+                yCurve = curve.replace('t', f'{t}')
+                return eval(yCurve)
+            
+            newPoints = [(xCurveFunction(0), yCurveFunction(0))] + splitCurve(xCurve=xCurveFunction, yCurve=yCurveFunction, n=4) + [(xCurveFunction(1), yCurveFunction(1))]
+            curvesSplitPoints += newPoints
+            splitPointsFile.writelines(f'{newPoints}'[1:-1] + '\n')
+        return curvesSplitPoints
+
+print(getSplitPoints())
